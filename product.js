347f8cbf1b9a113df9758df7a19e3d80682c9b5d
Het Soni <202307010069@karnavatiuniversity.edu.in>
2026-03-06 23:13:57 +0530

    Delete product.js

--- product.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const productSchema = new mongoose.Schema({
-  name:  { type: String, required: true, trim: true },
-  price: { type: Number, required: true, min: 0 },
-  stock: { type: Number, required: true, min: 0 },
-}, { timestamps: true }); // adds createdAt and updatedAt automatically
-
-module.exports = mongoose.model('Product', productSchema);
